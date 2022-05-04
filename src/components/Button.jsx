65fc9993b44577de3230617d58fd53abf983f7b8
Tyler Hougland <mtylerhouglandphd@gmail.com>
2022-05-04 13:09:00 -0500

    MadLibsDemo

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+function Button({label, action}){
+    return(
+        <button onClick={action}
+            className="border-2 border-green-900 p-4, rounded-xl bg-green-600 text-green-50">
+            {label}
+        </button>
+    );
+}
+
+export default Button;
---
+++ src/components/Story.jsx
@@ -0,0 +1,13 @@
+function Story({story}){
+    return(
+        <div>
+                <p>{story.character} was walking along one day in {story.setting}. {story.character} was
+                a world traveler, so he wanted to go to {story.place}. However, {story.character} wasn't able to
+                because {story.character} {story.problem}. {story.character} was very {story.adj1} and {story.adj2}. 
+                So {story.character} {story.action}, and interestingly {story.result}.
+                </p>
+        </div>
+    );
+}
+
+export default Story;
